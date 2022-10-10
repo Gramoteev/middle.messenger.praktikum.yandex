a23cad207b23b20459ac339e9d319bd930cc3315
Dmitry Gramoteev <gramoteev@gmail.com>
2022-10-10 12:01:33 +0500

    change CSS color variables

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import Block from 'core/block';
-import {validateFormElement} from "../../helpers/validate-form";
+import validateFormElement from "helpers/validate-form";
 
 import './profile.pcss';
 
@@ -13,8 +13,8 @@ export class ProfilePage extends Block {
         e.preventDefault();
 
         let isValid = true;
-        Object.entries(this.refs).forEach(fieldRef => {
-          const element = fieldRef[1].element?.querySelector(`#${fieldRef[0]}`) as HTMLInputElement;
+        Object.entries(this.refs).forEach(([name, field]) => {
+          const element = field.element?.querySelector(`#${name}`) as HTMLInputElement;
           const errorMessage = validateFormElement(element);
           this.refs[element.id].refs.errorRef.setProps({text: errorMessage});
           if (errorMessage) {
